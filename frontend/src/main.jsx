3b808e22bf5febac43edb4d0cbe50982c9161ef6
Mark Burenko <burenkosayenko@gmail.com>
2026-05-01 19:35:46 -0700

    add Elise's work into skeleton

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Test from './Test.jsx'
 import Login from './Login.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Test />
     <Login />
+    <App />
   </StrictMode>
 )
