7651a33708fba2f9afccb4b02dc24cc61d97b010
Mark Burenko <burenkosayenko@gmail.com>
2026-05-07 19:37:31 -0700

    allow fining bench description based on decimal coordinates

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const pool = mysql.createPool({
 
 app.use(
     cors({
-        origin: ["http://127.0.0.1:5173"]
+        origin: ["http://localhost:5173"]
     })
 )
 
--- frontend/src/Test.jsx
+++ frontend/src/Test.jsx
@@ -2,32 +2,43 @@ import { useState } from 'react'
 import './Test.css'
 
 function Test() {
-  const [ bench, setBench ] = useState("");
+  const [ description, setDescription ] = useState("");
 
-  async function getBench() {
+  async function getDescription(lat, long) {
     try {
-      const response = await fetch('http://localhost:8080/bench');
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-        console.log(`Got new bench: ${result}`);
-      }
+      const response = await fetch(`
+        http://localhost:8080/bench?lat=${lat}&long=${long}
+        `);
       const data = await response.json();
-      setBench(data.title);
+      setDescription(data.description);
     } catch(error) {
       console.log(`Error message: `, error);
     }
   }
 
-  async function resetBench() {
-    setBench( "" );
+  function resetDescription() {
+    setDescription( "" );
   }
 
+  const [lat, setLat ] = useState('');
+  const [ long, setLong ] = useState('');
+  
   return (
-    <>
-        <p>{bench}</p>
-        <button onClick={() => getBench()}>Press me!</button>
-        <button onClick={() => resetBench()}>Destroy!!!</button>
-    </>
+  <>
+    <input
+      type="text"
+      value={lat}
+      onChange={(e) => setLat(e.target.value)}
+    />
+    <input
+      type="text"
+      value={long}
+      onChange={(e) => setLong(e.target.value)}
+    />
+    <p>{description}</p>
+    <button onClick={() => getDescription(lat, long)}>Press me!</button>
+    <button onClick={() => resetDescription()}>Reset Description</button>
+  </>
   )
 }
 
