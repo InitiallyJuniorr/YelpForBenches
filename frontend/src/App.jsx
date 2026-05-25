f66d9c13b20fa4d2e52462a07403e8f6988b21cd
Mark Burenko <burenkosayenko@gmail.com>
2026-05-24 18:32:38 -0700

    make map fit screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <NavBar/>
       
       <section id="center">
-        <div className="page" style={{ display: 'block', width: '100%', height: '500px' }}>
+        <div className="page" style={{ display: 'block', width: '100%', height: '100%' }}>
           <MapPage />
         </div>
       </section>
